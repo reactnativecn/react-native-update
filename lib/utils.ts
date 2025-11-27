8b4a2b98d05f741fe64e31bceaa00a0e24314d29
sunnylqm <sunnylqm@gmail.com>
2025-11-27 14:57:52 +0800

    v9.2.8

--- lib/utils.ts
+++ lib/utils.ts
@@ -88,12 +88,6 @@ export const enhancedFetch = async (
   isRetry = false,
 ) => {
   return fetch(url, params)
-    .then((r) => {
-      if (r.ok) {
-        return r;
-      }
-      throw new Error(`${r.status} ${r.statusText}`);
-    })
     .catch((e) => {
       logger('fetch error', url, e);
       if (isRetry) {
