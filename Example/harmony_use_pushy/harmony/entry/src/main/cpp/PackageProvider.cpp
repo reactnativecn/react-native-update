c1f94a71d093d9e3df8b75b01365b3a47f99510c
sunnylqm <sunnylqm@gmail.com>
2026-03-16 21:27:02 +0800

    cleanup

--- Example/harmony_use_pushy/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ Example/harmony_use_pushy/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,20 +1,9 @@
 #include "RNOH/PackageProvider.h"
-
-#if __has_include("PushyPackage.h")
 #include "PushyPackage.h"
-#define HAS_PUSHY_PACKAGE 1
-#else
-#define HAS_PUSHY_PACKAGE 0
-#endif
-
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
-#if HAS_PUSHY_PACKAGE
     return {
          std::make_shared<PushyPackage>(ctx)
     };
-#else
-    return {};
-#endif
 }
