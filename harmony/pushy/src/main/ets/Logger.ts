933bfd4a41d3bd9288f532c26ba0787d8efc3afb
sunnylqm <sunnylqm@gmail.com>
2026-03-16 21:13:08 +0800

    refactor: Implement UpdateContext as a singleton and centralize preference and launch state management.

--- harmony/pushy/src/main/ets/Logger.ts
+++ harmony/pushy/src/main/ets/Logger.ts
@@ -1,4 +1,3 @@
-
 import hilog from '@ohos.hilog';
 
 class Logger {
@@ -7,29 +6,47 @@ class Logger {
   private format: string = '%{public}s,%{public}s';
   private isDebug: boolean;
 
-  constructor(prefix: string = 'MyApp', domain: number = 0xFF00, isDebug = false) {
+  constructor(
+    prefix: string = 'MyApp',
+    domain: number = 0xff00,
+    isDebug = false,
+  ) {
     this.prefix = prefix;
     this.domain = domain;
     this.isDebug = isDebug;
   }
 
+  private normalizeArgs(args: string[]): [string, string] {
+    if (args.length === 0) {
+      return ['', ''];
+    }
+    if (args.length === 1) {
+      return [args[0], ''];
+    }
+    return [args[0], args.slice(1).join(' ')];
+  }
+
   debug(...args: string[]): void {
     if (this.isDebug) {
-      hilog.debug(this.domain, this.prefix, this.format, args);
+      const [tag, message] = this.normalizeArgs(args);
+      hilog.debug(this.domain, this.prefix, this.format, tag, message);
     }
   }
 
   info(...args: string[]): void {
-    hilog.info(this.domain, this.prefix, this.format, args);
+    const [tag, message] = this.normalizeArgs(args);
+    hilog.info(this.domain, this.prefix, this.format, tag, message);
   }
 
   warn(...args: string[]): void {
-    hilog.warn(this.domain, this.prefix, this.format, args);
+    const [tag, message] = this.normalizeArgs(args);
+    hilog.warn(this.domain, this.prefix, this.format, tag, message);
   }
 
   error(...args: string[]): void {
-    hilog.error(this.domain, this.prefix, this.format, args);
+    const [tag, message] = this.normalizeArgs(args);
+    hilog.error(this.domain, this.prefix, this.format, tag, message);
   }
 }
 
-export default new Logger('geolocation', 0xFF00, false)
+export default new Logger('pushy', 0xff00, false);
