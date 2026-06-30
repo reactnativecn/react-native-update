7181b3d09716c391ffadb70fbeffca2752e8b113
sunnylqm <sunnylqm@gmail.com>
2026-06-30 14:34:10 +0800

    refactor: apply consistent indentation and formatting to core logic module

--- src/core.ts
+++ src/core.ts
@@ -1,58 +1,59 @@
-import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
-import { emptyModule, error, log } from './utils';
-import i18n from './i18n';
+import { NativeEventEmitter, NativeModules, Platform } from "react-native";
+import i18n from "./i18n";
+import { emptyModule, error, log } from "./utils";
+
 /* eslint-disable @react-native/no-deep-imports */
 const {
-  version: v,
-} = require('react-native/Libraries/Core/ReactNativeVersion');
+	version: v,
+} = require("react-native/Libraries/Core/ReactNativeVersion");
 const RNVersion = `${v.major}.${v.minor}.${v.patch}`;
 const isTurboModuleEnabled =
-  // https://github.com/facebook/react-native/pull/48362
-  (global as any).__turboModuleProxy || (global as any).RN$Bridgeless;
+	// https://github.com/facebook/react-native/pull/48362
+	(global as any).__turboModuleProxy || (global as any).RN$Bridgeless;
 
 export const PushyModule =
-  Platform.OS === 'web'
-    ? emptyModule
-    : isTurboModuleEnabled
-    ? require('./NativePushy').default
-    : NativeModules.Pushy;
+	Platform.OS === "web"
+		? emptyModule
+		: isTurboModuleEnabled
+			? require("./NativePushy").default
+			: NativeModules.Pushy;
 
 export const UpdateModule = PushyModule;
 
 if (!PushyModule) {
-  throw Error(
-    'Failed to load react-native-update native module, please try to recompile',
-  );
+	throw Error(
+		"Failed to load react-native-update native module, please try to recompile",
+	);
 }
 
 const PushyConstants = isTurboModuleEnabled
-  ? PushyModule.getConstants()
-  : PushyModule;
+	? PushyModule.getConstants()
+	: PushyModule;
 
 export const downloadRootDir: string = PushyConstants.downloadRootDir;
 export const packageVersion: string = PushyConstants.packageVersion;
 export const currentVersion: string = PushyConstants.currentVersion;
 
 export function setLocalHashInfo(hash: string, info: Record<string, any>) {
-  PushyModule.setLocalHashInfo(hash, JSON.stringify(info));
+	PushyModule.setLocalHashInfo(hash, JSON.stringify(info));
 }
 
 const currentVersionInfoString: string = PushyConstants.currentVersionInfo;
 let _currentVersionInfo: Record<string, any> = {};
 let isDebugChannel = false;
 if (currentVersionInfoString) {
-  try {
-    _currentVersionInfo = JSON.parse(currentVersionInfoString);
-    if (_currentVersionInfo.debugChannel) {
-      isDebugChannel = true;
-      delete _currentVersionInfo.debugChannel;
-      setLocalHashInfo(currentVersion, _currentVersionInfo);
-    }
-  } catch {
-    error(
-      i18n.t('error_parse_version_info', { info: currentVersionInfoString }),
-    );
-  }
+	try {
+		_currentVersionInfo = JSON.parse(currentVersionInfoString);
+		if (_currentVersionInfo.debugChannel) {
+			isDebugChannel = true;
+			delete _currentVersionInfo.debugChannel;
+			setLocalHashInfo(currentVersion, _currentVersionInfo);
+		}
+	} catch {
+		error(
+			i18n.t("error_parse_version_info", { info: currentVersionInfoString }),
+		);
+	}
 }
 export const currentVersionInfo = _currentVersionInfo;
 
@@ -64,25 +65,24 @@ export const isRolledBack: boolean = !!rolledBackVersion;
 export const buildTime: string = PushyConstants.buildTime;
 let uuid = PushyConstants.uuid;
 
-
 async function getLocalHashInfo(hash: string) {
-  return JSON.parse(await PushyModule.getLocalHashInfo(hash));
+	return JSON.parse(await PushyModule.getLocalHashInfo(hash));
 }
 
 // @deprecated use currentVersionInfo instead
 export async function getCurrentVersionInfo(): Promise<{
-  name?: string;
-  description?: string;
-  metaInfo?: string;
+	name?: string;
+	description?: string;
+	metaInfo?: string;
 }> {
-  return currentVersion ? (await getLocalHashInfo(currentVersion)) || {} : {};
+	return currentVersion ? (await getLocalHashInfo(currentVersion)) || {} : {};
 }
 
 export const pushyNativeEventEmitter = new NativeEventEmitter(PushyModule);
 
 if (!uuid) {
-  uuid = require('nanoid/non-secure').nanoid();
-  PushyModule.setUuid(uuid);
+	uuid = require("nanoid/non-secure").nanoid();
+	PushyModule.setUuid(uuid);
 }
 
 export const cInfo = {
@@ -92,7 +92,9 @@ export const cInfo = {
 	uuid,
 };
 
-log('bootup status', {
+log("bootup status", {
+	packageVersion,
+	currentVersion,
 	currentVersionInfo,
 	isFirstTime,
 	isFirstTimeDebug,
